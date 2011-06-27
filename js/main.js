9f993c9bdfb3e7df0d01a3e37324a471e7c0af8d
tension7 <tension7@gmail.com>
2011-06-26 22:52:51 -0700

    better sitekey guessing from domain name, also store previous sitekey changes in local storage.

--- js/main.js
+++ js/main.js
@@ -2,24 +2,39 @@ var $ = function(id) {
   return document.getElementById(id);
 }
 
-var getDefaultSiteKey = function() {
+var App = {
+
+'getDefaultSiteKey' : function() {
   if (!chrome || !chrome.tabs) {
     //not in extension mode
     return;
   }
+  var x = this;
   chrome.tabs.getSelected(null, function(tab){
-    reg=/:\/\/\w+\.(\w+)/;
-    res=reg.exec(tab.url);
-    if (res.length >= 2) {
-      $('site_key').value=res[1];
-      if ($('master_key').value) {
-        onSubmitSiteKey();
+    if (!tab.url) {
+      return;
+    }
+    var domain = tab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+    var sitekey = window.localStorage.getItem(domain);
+    if (!sitekey) {
+      var tokens = domain.split('.');
+      var topLevelDomain = /com|edu|gov|net|org|info/;
+      if (tokens.length <= 2 || tokens[1].match(topLevelDomain)) {
+        sitekey = tokens[0];
+      } else {
+        sitekey = tokens[1];
       }
     }
+    x.domain = domain;
+    x.sitekey = sitekey;
+    $('site_key').value = sitekey;
+    if ($('master_key').value) {
+      onSubmitSiteKey();
+    }
   });
-}
+},
 
-var onSubmitSiteKey = function() {
+'onSubmitSiteKey' : function() {
   var site = $('site_key').value;
   var master = $('master_key').value;
   var hmac = Crypto.util.bytesToBase64(Crypto.HMAC(Crypto.MD5, site, master, {asBytes:true}));
@@ -27,12 +42,17 @@ var onSubmitSiteKey = function() {
   $('output_key').value=hmac;
   $('output_key').focus();
   $('output_key').select();
-}
+  if (site && site != this.sitekey) {
+    window.localStorage.setItem(this.domain, site);
+  }
+},
 
-var setFocus = function() {
+setFocus : function() {
   if ($('master_key').value) {
     $('site_key').focus();
   } else {
     $('master_key').focus();
   }
-}
+},
+
+} // end of app
