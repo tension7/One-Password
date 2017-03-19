70dd25bc01a05b334caf84089af9d68ecc96266f
Li Zhang <tension7@gmail.com>
2017-03-19 10:19:19 -0700

    migrate to python27

--- build_package.sh
+++ build_package.sh
@@ -1,5 +1,5 @@
 mkdir onepwd
-cp popup.html CHANGELOG LICENSE manifest.json icon.png onepwd/
+cp popup.html CHANGELOG LICENSE manifest.json icon.png warning.png onepwd/
 mkdir onepwd/js
 cp js/2.2.0-crypto-md5.js js/2.2.0-crypto-min.js js/2.2.0-hmac-min.js js/main_mini.js onepwd/js
 zip -r onepwd.zip onepwd
